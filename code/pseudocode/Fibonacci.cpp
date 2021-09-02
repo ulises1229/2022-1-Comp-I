9ba1ea1015539589ed1a413185fcebba995825be
Ulises Olivares-Pinto <uolivares@unam.mx>
2021-09-01 20:42:42 -0500

    Add files via upload

---
+++ code/pseudocode/Fibonacci.cpp
@@ -0,0 +1,18 @@
+// Autor: Ulises Olivares
+// uolivares@unam.mx
+// 01 Sept, 2021
+
+// Algoritmo 1: Calcular la serie de Fibonacci hasta n = 100
+procedure calculteFibonacci(n): n = 100
+	//Input: n número de iteraciones
+	//Output: Array
+	fib[n]	// Definición de un arreglo
+	fib[0] = 1
+	fib[1] = 2
+	i = 2
+	while(i <= n):
+	 fib[i] = fib[i-2] + fib[i-1]
+	 i = i + 1
+	return fib
+
+	Complejidad: O(n)
