8db9090e1197acc43e660b180bd4d4e300e7efe2
Ulises Olivares-Pinto <uolivares@unam.mx>
2021-09-08 12:21:56 -0500

    Add code: Arithmetic operators

---
+++ code/C++/arithmetic_operators.cpp
@@ -0,0 +1,36 @@
+/*
+ * Autor: Ulises Olivares
+ * uolivares@unam.mx
+ * 03 Sept, 2021
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    /* Definición de variables enteras */
+    int a = 50, b = 26;
+    //int x = a + b + c;
+
+    // Definición de variables flotantes
+    float d = 2.99, e = 2.141596;
+
+    // Operador de suma
+    cout << "La suma de " << a << " + " << b << " es iguial a: " <<  a + b  << endl;
+
+    // Operador de resta
+    cout << "La resta de " << a << " - " << b << " es iguial a: " <<  a - b  << endl;
+
+    // Operador de multiplicación
+    cout << "La multiplicación de " << d << " * " << e << " es iguial a: " <<  d * e  << endl;
+
+    // Operador de división
+    cout << "La división de " << d << " / " << e << " es iguial a: " <<  d / e  << endl;
+
+    // Operador de división
+    cout << "El módulo de " << a << " % " << b << " es iguial a: " <<  a % b  << endl;
+
+    return 0;
+}
